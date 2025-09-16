716896d0af8d586884547bf5f31ebc294dfcfd4c
aleksy.anishchenko <aleksy.anishchenko@gmail.com>
2025-09-16 21:58:43 +0700

    Extended project typing

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "@/router/router.ts";
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
 import "@/styles/global.css";
 
 const app = createApp(App)
 
 const pinia = createPinia()
+const vuetify = createVuetify()
+
 
 app
     .use(pinia)
     .use(router)
+    .use(vuetify)
     .mount('#app')
